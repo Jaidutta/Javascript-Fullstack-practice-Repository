d5613e630a88fcddbf325288a91354fe2c98d222
Joy Dutta <dutta_joy@hotmail.com>
2026-05-08 23:56:31 +0100

    More on Node and Bun JS

---
+++ 6.1 Extra Materials/1. Node js, Bun, Node js Runtime/practise node/index.mjs
@@ -0,0 +1,6 @@
+import chalk from 'chalk'; // modern import syntax
+
+const log = console.log;
+log(chalk.blue('Hello, World!'));
+log(chalk.red.bold("This is an error message."));
+
---
+++ 6.1 Extra Materials/1. Node js, Bun, Node js Runtime/practise node/main.js
@@ -0,0 +1,7 @@
+const path = require('path');
+console.log(__dirname);
+console.log(path.join(__dirname, "main.js"));
+
+console.log(__dirname + "/index.js");
+
+console.log(path.join(__dirname, "../../index.js", "/projects", "/index.js"));
